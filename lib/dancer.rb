c818e2b94f8e71aab77e8958fb0bf38319a1e34d
Dario Carlino <baires82@gmail.com>
2020-09-11 14:23:01 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
+  include Fancydance
+  extend FancyDance
+
 
   attr_accessor :name
 
